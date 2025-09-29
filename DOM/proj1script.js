b11be75c69556098197b40fa00fd69e7f875aa70
Anuj srivastav <anujsrivastav683@gmail.com>
2025-09-29 00:30:10 -0700

    learning DOM and event and making project

---
+++ DOM/proj1script.js
@@ -0,0 +1,41 @@
+
+const redColor = document.querySelector(".red");
+const grayColor = document.getElementsByClassName("gray");
+const blackColor = document.querySelector(".black");
+const cartButton = document.getElementById("button");
+const itemTag = document.getElementsByTagName("h3")[0];
+const imageCard = document.querySelector(".product-image");
+const feedBackbtn = document.querySelector(".feedback"); 
+
+redColor.addEventListener("click", function () {
+    cartButton.style.backgroundColor = "red";
+    itemTag.style.backgroundColor = "red";
+    imageCard.style.backgroundImage = 'url("/DOM/red thar.avif")';
+})
+
+grayColor[0].addEventListener("click", function () {
+  cartButton.style.backgroundColor = "gray";
+  itemTag.style.backgroundColor = "gray";
+  imageCard.style.backgroundImage = 'url("/DOM/grayThar.jpg")';
+});
+
+blackColor.addEventListener("click", function () {
+  cartButton.style.backgroundColor = "black";
+  itemTag.style.backgroundColor = "black";
+  imageCard.style.backgroundImage = 'url("/DOM/blackImage.avif")';
+});
+
+const cart = () => {
+    cartButton.style.display = "none";
+    feedBackbtn.style.display = "block";
+}
+
+cartButton.addEventListener("click", cart); 
+
+const feedback = () => {
+    cartButton.style.display = "block";
+    cartButton.style.display = "none";
+}
+
+feedBackbtn.addEventListener("click", feedback);
+
--- Loops and Method in Js/array.js
+++ Loops and Method in Js/array.js
@@ -27,18 +27,50 @@
 
 // create array with some default values using Array.fills
 
-const arr = new Array(3).fill(0);
-console.log(arr);
+// const arr = new Array(3).fill(0);
+// console.log(arr);
 
 // after updating the values of the array values get updated.
-arr[0] = 1;
-arr[1] = 3;
-arr[2] = 4;
+// arr[0] = 1;
+// arr[1] = 3;
+// arr[2] = 4;
 
+// console.log(arr);
+
+// let arr2 = new Array(4);
+// console.log(arr2);
+
+let arr = Array;
 console.log(arr);
 
-let arr2 = new Array(4);
+let arr1 = new Array();
+console.log(arr1);
+
+let arr2 = Array();
 console.log(arr2);
+
+
+
+
+
+
+
+function add() {
+    console.log("hi");
+    
+}
+
+console.log(typeof add
+
+);
+
+
+
+class Something{
+    Something() {
+
+    }
+}
 
 
 
